ce2fb304db778d235733e4f0a272199ad5894277
Justin Hall <hall.justin.d@gmail.com>
2019-03-22 20:04:54 +0000

    first commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+# Specify your gem's dependencies in pod_scraper.gemspec
+gemspec
---
+++ lib/pod_scraper.rb
@@ -0,0 +1,6 @@
+require "pod_scraper/version"
+
+module PodScraper
+  class Error < StandardError; end
+  # Your code goes here...
+end
